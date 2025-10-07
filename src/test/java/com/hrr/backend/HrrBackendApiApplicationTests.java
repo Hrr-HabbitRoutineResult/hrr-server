dfda86c0ddb0a6482c88754f73253be7c5bb6fb2
yc3697 <youngchan9683@gmail.com>
2025-10-07 21:42:22 +0900

    init: spring boot 세팅

---
+++ src/test/java/com/hrr/backend/HrrBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrrBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
