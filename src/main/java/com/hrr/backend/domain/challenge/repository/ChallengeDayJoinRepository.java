eb1f345dde7fb321983f1453ddad17369371958d
yc3697 <youngchan9683@gmail.com>
2025-11-04 00:33:50 +0900

    feat: 요일 정보도 포함되게 수정 #19

---
+++ src/main/java/com/hrr/backend/domain/challenge/repository/ChallengeDayJoinRepository.java
@@ -0,0 +1,11 @@
+package com.hrr.backend.domain.challenge.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hrr.backend.domain.challenge.entity.ChallengeDayJoin;
+
+public interface ChallengeDayJoinRepository extends JpaRepository<ChallengeDayJoin, Long> {
+	List<ChallengeDayJoin> findByChallengeIdIn(List<Long> challengeIds);
+}
--- src/main/java/com/hrr/backend/domain/challenge/service/ChallengeServiceImpl.java
+++ src/main/java/com/hrr/backend/domain/challenge/service/ChallengeServiceImpl.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 import com.hrr.backend.domain.challenge.dto.ChallengeResponseDto;
+import com.hrr.backend.domain.challenge.entity.ChallengeDayJoin;
+import com.hrr.backend.domain.challenge.repository.ChallengeDayJoinRepository;
 import com.hrr.backend.domain.challenge.repository.ChallengeRepository;
 import com.hrr.backend.global.common.enums.Category;
 import com.hrr.backend.global.common.enums.ChallengeDays;
@@ -24,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class ChallengeServiceImpl implements ChallengeService {
 
 	private final ChallengeRepository challengeRepository;
+	private final ChallengeDayJoinRepository challengeDayJoinRepository;
 
 	private static final int UPCOMING_DAYS_CRITERIA = 5;	// '곧 시작' 챌린지 판단 기준 일자
 
@@ -54,7 +59,7 @@ public class ChallengeServiceImpl implements ChallengeService {
 
 		Pageable pageable = PageRequest.of(page, size);
 
-		// Repository 호출
+		// Repository 호출 - 필터 적용된 챌린지들 반환(요일, 곧 시작 여부, 시작까지 남은 일자는 제외)
 		Slice<ChallengeResponseDto.InfoDto> tempDtoSlice = challengeRepository.findChallengesWithFilters(
 			category,
 			upcomingStartDate,
@@ -65,6 +70,23 @@ public class ChallengeServiceImpl implements ChallengeService {
 			pageable
 		);
 
+		// ---응답 dto에 추가할 요일 정보 조회---
+		// 모든 challengeId 추출
+		List<Long> challengeIds = tempDtoSlice.getContent().stream()
+			.map(ChallengeResponseDto.InfoDto::getChallengeId)
+			.toList();
+
+		// 해당 challenge들을 가진 ChallengeDayJoin 엔티티 조회
+		List<ChallengeDayJoin> allDays = challengeDayJoinRepository.findByChallengeIdIn(challengeIds);
+
+		// 챌린지 아이디-요일 리스트 매핑
+		Map<Long, List<ChallengeDays>> daysMap = allDays.stream()
+			.collect(Collectors.groupingBy(
+				dayJoin -> dayJoin.getChallenge().getId(), // key: 챌린지 아이디
+				Collectors.mapping(ChallengeDayJoin::getDayOfWeek, Collectors.toList()) // value: 요일 리스트
+			));
+
+
 		// Repository에서 조회해 온 dto 기반으로 결과 dto 필드 일부 채우기
 		Slice<ChallengeResponseDto.InfoDto> finalDtoSlice = tempDtoSlice.map(tempDto -> {
 
@@ -74,9 +96,10 @@ public class ChallengeServiceImpl implements ChallengeService {
 			long dDay = ChronoUnit.DAYS.between(today, challengeStartDate);
 			boolean isUpcomingResult = (dDay >= 0) && (dDay <= UPCOMING_DAYS_CRITERIA);
 
-			// dto 나머지 필드 채우기(isUpcoming, dDayUntilStart)
+			// dto 나머지 필드 채우기(isUpcoming, dDayUntilStart, dayOfWeek)
 			tempDto.setIsUpcoming(isUpcomingResult);
 			tempDto.setDDayUntilStart((int)Math.max(0, dDay));	// 시작 전일 경우에만 남은 날짜를 set, else 0
+			tempDto.setDaysOfWeek(daysMap.getOrDefault(tempDto.getChallengeId(), List.of()));
 
 			return tempDto;
 		});
