a06c93df47f264a02e8b37a82c6a5f21bdc6a961
yc3697 <youngchan9683@gmail.com>
2025-11-16 12:19:49 +0900

    chore: 조회 개수에 제한 추가 및 에러 응답 수정 #37

--- src/main/java/com/hrr/backend/domain/challenge/controller/ChallengeController.java
+++ src/main/java/com/hrr/backend/domain/challenge/controller/ChallengeController.java
@@ -2,6 +2,7 @@ package com.hrr.backend.domain.challenge.controller;
 
 import java.util.List;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +22,15 @@ import com.hrr.backend.global.response.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Challenge", description = "챌린지 관련 API")
 @RestController
 @RequestMapping("/api/v1/challenges")
 @RequiredArgsConstructor
+@Validated
 public class ChallengeController {
 
 	private final ChallengeService challengeService;
@@ -77,7 +81,10 @@ public class ChallengeController {
 
 	@GetMapping("/daily-top")
 	@Operation(summary = "오늘의 인기 챌린지 조회", description = "오늘의 클릭 수를 기준으로 상위 n개 챌린지를 조회합니다.")
-	public ApiResponse<List<ChallengeResponseDto.DailyTopDto>> getDailyTopChallenges(@RequestParam("number") int number) {
+	public ApiResponse<List<ChallengeResponseDto.DailyTopDto>> getDailyTopChallenges(
+		@RequestParam("number")
+		@Min(value = 1, message = "조회 개수는 1 이상이어야 합니다")
+		@Max(value = 50, message = "성능상 조회 개수는 50 이하로 제한하고 있습니다.") int number) {
 
 		return ApiResponse.onSuccess(SuccessCode.OK, challengeService.getDailyTopChallenges(number));
 	}
