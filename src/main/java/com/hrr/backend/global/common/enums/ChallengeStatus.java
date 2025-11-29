d0715a6b3550238c909a486010639e735d16097b
yangjiae12 <yangjiae12@gmail.com>
2025-11-30 01:56:35 +0900

    fix: ChallengeStatus Enum 오타 수정 #55
    - RECRUTING -> RECRUITING 스펠링 수정

--- src/main/java/com/hrr/backend/global/common/enums/ChallengeStatus.java
+++ src/main/java/com/hrr/backend/global/common/enums/ChallengeStatus.java
@@ -3,6 +3,6 @@ package com.hrr.backend.global.common.enums;
 public enum ChallengeStatus {
 	UPCOMING,	// 챌린지 시작 전
 	ONGOING,	// 라운드 진행 중 (가입 불가)
-	RECRUTING,	// 다음 라운드 모집 중
+	RECRUITING,	// 다음 라운드 모집 중
 	FINISHED	// 챌린지 종료
 }
