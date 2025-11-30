2f189dd348ed092df8bea4b6dd8ddeb019b24ecc
yc3697 <youngchan9683@gmail.com>
2025-12-01 00:46:44 +0900

    chore: 불필요한 주석 제거 #73

--- src/main/java/com/hrr/backend/global/config/S3Config.java
+++ src/main/java/com/hrr/backend/global/config/S3Config.java
@@ -13,11 +13,6 @@ public class S3Config {
 	@Value("${aws.s3.region}")
 	private String s3Region;
 
-	/**
-	 * Presigned URL 발급을 위한 S3Presigner Bean 등록
-	 * * AWS SDK는 EC2에 연결된 IAM 역할(Role)이나
-	 * 환경 변수에서 자격 증명을 자동으로 찾기 때문에 별도의 .credentialsProvider() 설정이 필요 없습니다.
-	 */
 	@Bean
 	public S3Presigner s3Presigner() {
 		// IAM의 role로 credentials를 대체
