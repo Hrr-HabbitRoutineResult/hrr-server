0557b4da7b19b5e1b813ac34679487cddfa0ccc9
Yerin Chun <20211103@sungshin.ac.kr>
2025-12-03 17:25:12 +0900

    Fix: 잘못된 import 수정 #85

--- src/main/java/com/hrr/backend/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/hrr/backend/domain/follow/repository/FollowRepository.java
@@ -2,7 +2,7 @@ package com.hrr.backend.domain.follow.repository;
 
 import com.hrr.backend.domain.follow.entity.Follow;
 import com.hrr.backend.domain.user.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
