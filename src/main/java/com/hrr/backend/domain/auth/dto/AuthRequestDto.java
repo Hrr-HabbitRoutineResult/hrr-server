729fa82250a8131da81fdbc8203c85d1ee8c8575
yc3697 <youngchan9683@gmail.com>
2025-12-05 14:03:26 +0900

    feat: 카카오 엑세스 토큰을 활용한 로그인 방식 추가 #95

--- src/main/java/com/hrr/backend/domain/auth/dto/AuthRequestDto.java
+++ src/main/java/com/hrr/backend/domain/auth/dto/AuthRequestDto.java
@@ -1,10 +1,19 @@
 package com.hrr.backend.domain.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 
 public class AuthRequestDto {
     public record SocialLoginRequest(
             @NotBlank(message = "인가 코드는 필수입니다.")
             String code
     ) {}
+
+	@Getter
+	public static class KakaoAccessTokenDto {
+		@Schema(description = "카카오 SDK에서 발급받은 Access Token", example = "string")
+		@NotBlank(message = "토큰은 필수값입니다.")
+		String accessToken;
+	}
 }
