45eca6c61ba1b7a3490f95ba7e09602e28f072ad
yc3697 <youngchan9683@gmail.com>
2025-12-06 11:15:21 +0900

    Feat: 팔로우/팔로우 취소 API #83
    # Conflicts:
    #	src/main/java/com/hrr/backend/global/response/ErrorCode.java

--- src/main/java/com/hrr/backend/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/hrr/backend/domain/follow/repository/FollowRepository.java
@@ -3,7 +3,11 @@ package com.hrr.backend.domain.follow.repository;
 import com.hrr.backend.domain.follow.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
    boolean existsByFollowerIdAndFollowingId(Long followerId, Long followingId);
+
+   Optional<Follow> findByFollowerIdAndFollowingId(Long followerId, Long followingId);
 }
--- src/main/java/com/hrr/backend/global/response/ErrorCode.java
+++ src/main/java/com/hrr/backend/global/response/ErrorCode.java
@@ -69,9 +69,14 @@ public enum ErrorCode implements BaseCode{
     INVALID_TERM_ID_IN_REQUEST(HttpStatus.BAD_REQUEST, "TERM4004", "요청에 유효하지 않은 약관 ID가 포함되어 있습니다."),
     TERM_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "TERM5005", "약관 동의 저장 중 오류가 발생했습니다."),
 
+    // follow
+    CANNOT_FOLLOW_SELF(HttpStatus.BAD_REQUEST, "FOLLOW4001", "자기 자신을 팔로우할 수 없습니다."),
+    ALREADY_FOLLOWING(HttpStatus.CONFLICT, "FOLLOW4091", "이미 팔로우 중인 사용자입니다."),
+    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "FOLLOW4041", "팔로우 관계를 찾을 수 없습니다."),
+
 	// search
 	MIGRATION_REDIS_TO_DB_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SEARCH5001", "redis에서 DB로 로그를 저장하는 데 실패했습니다."),
-	COUNTING_LOG_TABLE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SEARCH5002", "집계가 실패하였습니다.")
+	COUNTING_LOG_TABLE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SEARCH5002", "집계가 실패하였습니다."),
     ;
 
 	private final HttpStatus status;
