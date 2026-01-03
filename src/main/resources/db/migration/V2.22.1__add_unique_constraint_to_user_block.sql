f4aff91a4444b06af43d5c27921499f01452c49b
yc3697 <youngchan9683@gmail.com>
2026-01-03 10:59:10 +0900

    chore: 마이그레이션 파일 중복 데이터 대비 추가 #173

--- src/main/resources/db/migration/V2.22.1__add_unique_constraint_to_user_block.sql
+++ src/main/resources/db/migration/V2.22.1__add_unique_constraint_to_user_block.sql
@@ -4,7 +4,15 @@ DROP PROCEDURE IF EXISTS AddUniqueConstraintToUserBlock $$
 
 CREATE PROCEDURE AddUniqueConstraintToUserBlock()
 BEGIN
-    -- 1. user_block 테이블에서 blocker_id와 blocked_id 조합의 유니크 인덱스가 없는 경우에만 실행
+    -- 1. 중복 데이터 먼저 제거
+    -- 동일한 blocker_id, blocked_id 조합이 여러 개라면, 가장 오래된 것만 유지
+    DELETE b1 FROM user_block b1
+                       INNER JOIN user_block b2
+    WHERE b1.id > b2.id
+      AND b1.blocker_id = b2.blocker_id
+      AND b1.blocked_id = b2.blocked_id;
+
+    -- 2. user_block 테이블에서 blocker_id와 blocked_id 조합의 유니크 인덱스가 없는 경우에만 실행
     IF NOT EXISTS (
         SELECT 1
         FROM information_schema.STATISTICS
@@ -12,7 +20,7 @@ BEGIN
           AND TABLE_NAME = 'user_block'
           AND INDEX_NAME = 'uk_blocker_blocked'
     ) THEN
-        -- 2. 유니크 제약 조건 추가 (인덱스 명: uk_blocker_blocked)
+        -- 3. 유니크 제약 조건 추가 (인덱스 명: uk_blocker_blocked)
         ALTER TABLE user_block
             ADD CONSTRAINT uk_blocker_blocked UNIQUE (blocker_id, blocked_id);
     END IF;
