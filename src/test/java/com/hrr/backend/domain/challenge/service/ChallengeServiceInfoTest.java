f3401cf7b9f4a566c216a2b6c96cd421bfd4ce41
yangjiae12 <yangjiae12@gmail.com>
2026-01-04 00:35:03 +0900

    test: 챌린지 정보 조회 테스트 안정화 및 시간 의존성 오류 수정 #174
    - LocalTime.now() 기반의 상대적 시간 계산을 고정 시간 및 MIN/MAX 상수로 교체
    - 자정 전후 테스트 실행 시 발생하던 시간 역전(start > end) 버그 해결
    - 테스트 데이터 생성 시 비즈니스 규칙(인증 종료 시간 > 시작 시간)을 준수하도록 수정

--- src/test/java/com/hrr/backend/domain/challenge/service/ChallengeServiceInfoTest.java
+++ src/test/java/com/hrr/backend/domain/challenge/service/ChallengeServiceInfoTest.java
@@ -63,7 +63,7 @@ class ChallengeServiceInfoTest {
         // 조건: 상태는 UPCOMING
         setChallengeStatus(challenge, ChallengeStatus.UPCOMING, 10, 30);
         // 조건: 인증 시간대 내 (시간은 맞지만 날짜가 안 맞음)
-        setVerificationTime(challenge, LocalTime.now().minusHours(1), LocalTime.now().plusHours(1));
+        setVerificationTime(challenge, LocalTime.of(9, 0), LocalTime.of(18, 0));
 
         // Mocking
         mockFetchingChallenge(challengeId, challenge);
@@ -92,7 +92,7 @@ class ChallengeServiceInfoTest {
 
         // 조건: 라운드 시작됨, 시간 맞음
         setRoundDate(challenge, LocalDate.now().minusDays(1));
-        setVerificationTime(challenge, LocalTime.now().minusHours(1), LocalTime.now().plusHours(1));
+        setVerificationTime(challenge, LocalTime.of(9, 0), LocalTime.of(18, 0));
         setChallengeStatus(challenge, ChallengeStatus.ONGOING, 10, 30);
 
         // Mocking
@@ -117,8 +117,8 @@ class ChallengeServiceInfoTest {
 
         setRoundDate(challenge, LocalDate.now().minusDays(1));
 
-        // 조건: 인증 시간이 현재 시간보다 미래임 (아직 안 옴)
-        setVerificationTime(challenge, LocalTime.now().plusHours(1), LocalTime.now().plusHours(2));
+        // 현재 시간과 겹치지 않도록 이른 시간대 고정
+        setVerificationTime(challenge, LocalTime.of(0, 0), LocalTime.of(0, 1));
 
         // Mocking
         mockFetchingChallenge(challengeId, challenge);
@@ -142,7 +142,7 @@ class ChallengeServiceInfoTest {
 
         setRoundDate(challenge, LocalDate.now().minusDays(1));
         // 조건: 현재 시간이 인증 시간 내에 포함됨
-        setVerificationTime(challenge, LocalTime.now().minusHours(1), LocalTime.now().plusHours(1));
+        setVerificationTime(challenge, LocalTime.MIN, LocalTime.MAX);
 
         // Mocking
         mockFetchingChallenge(challengeId, challenge);
@@ -169,7 +169,7 @@ class ChallengeServiceInfoTest {
         Challenge challenge = createChallengeBase();
 
         setRoundDate(challenge, LocalDate.now().minusDays(1));
-        setVerificationTime(challenge, LocalTime.now().minusHours(1), LocalTime.now().plusHours(1));
+        setVerificationTime(challenge, LocalTime.MIN, LocalTime.MAX);
 
         // Mocking
         mockFetchingChallenge(challengeId, challenge);
