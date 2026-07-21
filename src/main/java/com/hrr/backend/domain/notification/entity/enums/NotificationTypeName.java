36f46332257ed74496b8c64b5e775e766d8eb7be
yangjiae12 <yangjiae12@gmail.com>
2026-07-21 13:56:57 +0900

    refactor: CHALLENGE_VACANCY 알림 타입 추가#338

--- src/main/java/com/hrr/backend/domain/notification/entity/enums/NotificationTypeName.java
+++ src/main/java/com/hrr/backend/domain/notification/entity/enums/NotificationTypeName.java
@@ -3,6 +3,7 @@ package com.hrr.backend.domain.notification.entity.enums;
 public enum NotificationTypeName {
     CHALLENGE_START,              // 챌린지 시작 하루 전 안내
     CHALLENGE_UPDATED,            // 챌린지 수정 안내
+    CHALLENGE_VACANCY,            // 챌린지 빈자리 발생 안내
     CHALLENGE_EXTENSION,          // 챌린지 라운드 연장 안내
     CHALLENGE_EXTENSION_SUCCESS,  // 챌린지 연장 확정
     CHALLENGE_EXTENSION_CANCEL,   // 챌린지 연장 취소
