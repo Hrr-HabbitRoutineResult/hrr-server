7bea4fb4be6146ee7ce2ddeb84f3d3a074ef4b90
yangjiae12 <yangjiae12@gmail.com>
2026-07-22 01:05:23 +0900

    chore: 미사용 NotificationType 주석 추가 #338

--- src/main/java/com/hrr/backend/domain/notification/entity/enums/NotificationTypeName.java
+++ src/main/java/com/hrr/backend/domain/notification/entity/enums/NotificationTypeName.java
@@ -5,8 +5,8 @@ public enum NotificationTypeName {
     CHALLENGE_UPDATED,            // 챌린지 수정 안내
     CHALLENGE_VACANCY,            // 챌린지 빈자리 발생 안내
     CHALLENGE_EXTENSION,          // 챌린지 라운드 연장 안내
-    CHALLENGE_EXTENSION_SUCCESS,  // 챌린지 연장 확정
-    CHALLENGE_EXTENSION_CANCEL,   // 챌린지 연장 취소
+    CHALLENGE_EXTENSION_SUCCESS,  // Deprecated: 챌린지 연장 확정
+    CHALLENGE_EXTENSION_CANCEL,   // Deprecated: 챌린지 연장 취소
 
     VERIFICATION_DEADLINE_3H,     // 인증 마감 3시간 전
     VERIFICATION_DEADLINE_1H,     // 인증 마감 1시간 전
