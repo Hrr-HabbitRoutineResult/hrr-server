d8c994fdc4e6c8794386186fcaf403725b4d50b0
minkyung <HaMming.bright@gmail.com>
2026-08-19 17:52:29 +0900

    feat: 인증 상태에 삭제 추가

--- src/main/java/com/hrr/backend/domain/verification/entity/enums/VerificationStatus.java
+++ src/main/java/com/hrr/backend/domain/verification/entity/enums/VerificationStatus.java
@@ -3,5 +3,7 @@ package com.hrr.backend.domain.verification.entity.enums;
 public enum VerificationStatus {
     TEMPORARY,
     COMPLETED,
-	BLOCKED
+	BLOCKED,
+    // 부실인증 신고 이력 보존을 위함
+    DELETED
 }
