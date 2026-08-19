cc28458c07c14fd6b2fcc8a361e2841e81722905
minkyung <HaMming.bright@gmail.com>
2026-08-19 17:55:39 +0900

    feat: 삭제했던 필드 추가

--- src/main/java/com/hrr/backend/domain/verification/converter/VerificationConverter.java
+++ src/main/java/com/hrr/backend/domain/verification/converter/VerificationConverter.java
@@ -122,6 +122,8 @@ public class VerificationConverter {
     ) {
         return VerificationResponseDto.MyProfileDto.builder()
                 .nickname(userChallenge.getUser().getDisplayNickname())
+                //  TODO: 프론트 전환 완료 확인 후 DTO 필드와 함께 제거할 것.
+                .totalVerificationCount(verificationCount.longValue())
                 .verificationCount(verificationCount)
                 // 현재 라운드 부실 인증 횟수
                 .weakVerificationCount(weakVerificationCount)
