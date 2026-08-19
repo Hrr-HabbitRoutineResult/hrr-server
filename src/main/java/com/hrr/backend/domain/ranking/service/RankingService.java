8750d726ee8ae0fa961cbf111f5e394e129e28b1
minkyung <HaMming.bright@gmail.com>
2026-08-19 13:19:53 +0900

    feat: 주간 랭킹 스냅샷을 보장

--- src/main/java/com/hrr/backend/domain/ranking/service/RankingService.java
+++ src/main/java/com/hrr/backend/domain/ranking/service/RankingService.java
@@ -1,5 +1,7 @@
 package com.hrr.backend.domain.ranking.service;
 
+import java.time.LocalDate;
+
 import com.hrr.backend.domain.ranking.dto.RankingResponseDto;
 import com.hrr.backend.domain.user.entity.User;
 
@@ -7,4 +9,8 @@ public interface RankingService {
 
     // 내 랭킹 보드 조회 (상단 프로필은 실시간, 랭킹보드는 주간 스냅샷 기준)
     RankingResponseDto.BoardDto getMyRankingBoard(User user);
+
+    // 주간 랭킹 스냅샷 보장(catch-up)
+    /** 지정한 월요일자 스냅샷이 없으면 생성한다. (이미 있으면 아무것도 하지 않음) */
+    int ensureWeeklySnapshot(LocalDate snapshotDate);
 }
