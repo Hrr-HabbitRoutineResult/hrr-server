93dbce47a14b5c4993ccdf2a12c225ea4ead8de1
minkyung <HaMming.bright@gmail.com>
2026-08-19 18:31:31 +0900

    feat: migration 파일 수정

--- src/main/resources/db/migration/V2.46__recalculate_verification_and_warn_count.sql
+++ src/main/resources/db/migration/V2.46__recalculate_verification_and_warn_count.sql
@@ -3,6 +3,8 @@ SET rr.verification_count = (
     SELECT COUNT(*)
     FROM verification v
     WHERE v.round_record_id = rr.id
+      -- soft delete된 인증글은 집계에서 제외
+      AND v.status <> 'DELETED'
 );
 
 UPDATE round_record rr
